6441afbd8967adef7f1f4bb62c7627c8161cd461
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-12-03 13:47:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-function preload() {
+function preload() 
+{
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
-function setup() {
+function setup()
+ {
 	canvas = createCanvas(1240,336);
+	
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(1240,336);
+    video.parent("game_console");
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose', gotposes);
+
 }
-
-function draw() {
-	game()
-}
-
-
-
-
-
 
